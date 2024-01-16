81292b4754edc43a2ba9829ad8b7da990ae4ba61
Edwin <edwinj.gutierrezm@gmail.com>
2024-01-15 17:53:18 -0800

    admin  models

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Driver, Vehicle, InsuranceApplication
+
+admin.site.register(Driver)
+admin.site.register(Vehicle)
+admin.site.register(InsuranceApplication)
